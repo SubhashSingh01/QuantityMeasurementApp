2ca48c1528b0c87e440025c67816a55682cdd6ae
SubhashSingh01 <sobbythakur8805@gmail.com>
2026-02-24 11:20:50 +0530

    [Subhash] Add. Added UC2 problem of QuantityMeasurementApp

--- src/main/java/com/subhash/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/subhash/quantitymeasurement/QuantityMeasurementApp.java
@@ -24,15 +24,48 @@ public class QuantityMeasurementApp {
 			return Objects.hash(value);
 		}
 	}
-
-	public static void main(String[] args) {
+	public static class Inches{
+        private final double value;
+		
+		public Inches(double value) {
+			this.value=value;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if(this==obj) return true;
+			if(obj==null || getClass()!=obj.getClass()) return false;
+			Inches other=(Inches) obj;
+			return Double.compare(this.value, other.value)==0;
+		}
+		
+		@Override
+		public int hashCode() {
+			return Objects.hash(value);
+		}
+	}
+	public static void demonstrateFeetEquality() {
 		Feet feet1=new Feet(88.0);
 		Feet feet2=new Feet(88.0);
 		if(feet1.equals(feet2)) {
-			System.out.println("Both quantity are equal (True)");
+			System.out.println("Both quantity(Feet) are equal (True)");
 		}else {
-			System.out.println("Both quantity are not equal (False)");
+			System.out.println("Both quantity(Feet) are not equal (False)");
 		}
+	}
+	public static void demonstrateInchesEquality() {
+		Inches inch1=new Inches(10.0);
+		Inches inch2=new Inches(20.0);
+		if(inch1.equals(inch2)) {
+			System.out.println("Both quantities(Inches) are equal (True)");
+		}
+		else
+			System.out.println("Both quantities(Inches) are not equal (False)");
+	}
+
+	public static void main(String[] args) {
+		demonstrateFeetEquality();
+		demonstrateInchesEquality();
 	}
 
 }
