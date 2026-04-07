36069b7852f2287051edbdc1b6ca8d996005cafc
SubhashSingh01 <sobbythakur8805@gmail.com>
2026-04-07 16:58:01 +0530

    [Subhash] Add. Added to UC17 Spring-Framework-Integration of QMA

--- src/main/java/com/subhash/quantitymeasurement/exception/DatabaseException.java
+++ src/main/java/com/subhash/quantitymeasurement/exception/DatabaseException.java
@@ -7,8 +7,7 @@ public class DatabaseException extends RuntimeException {
 	 */
 	private static final long serialVersionUID = 1L;
 
-
-	public DatabaseException(String message) { super(message); }
+    public DatabaseException(String message) { super(message); }
 
     public DatabaseException(String message, Throwable cause) { super(message, cause); }
 }
---
+++ src/main/java/com/subhash/quantitymeasurement/model/OperationType.java
@@ -0,0 +1,9 @@
+package com.subhash.quantitymeasurement.model;
+
+public enum OperationType {
+	ADD,
+	SUBTRACT,
+	DIVIDE,
+	COMPARE,
+	CONVERT;
+}
